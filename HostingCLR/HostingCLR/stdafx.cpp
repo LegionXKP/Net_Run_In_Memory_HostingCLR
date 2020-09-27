1434c52717a77658a3062a677bea8c64cf21d4e7
r3xq1 <40786755+r3xq1@users.noreply.github.com>
2020-09-27 17:05:53 +0300

    Add files via upload

---
+++ HostingCLR/HostingCLR/stdafx.cpp
@@ -0,0 +1,17 @@
+#define _WIN32_WINNT 0x0500
+
+// stdafx.cpp: source file just including standard include files
+// HostingCLR.pch represents the precompiled header
+// stdafx.obj contains the precompiled type information
+
+// stdafx.cpp: исходный файл, включающий только стандартные включаемые файлы
+// HostingCLR.pch представляет предварительно скомпилированный заголовок
+// stdafx.obj содержит предварительно скомпилированную информацию о типе
+
+#include "stdafx.h"
+
+// TODO: refer to additional headers needed in STDAFX.H
+// missing from this file
+
+// TODO: обратитесь к дополнительным заголовкам, необходимым в STDAFX.H
+// отсутствует в этом файле
